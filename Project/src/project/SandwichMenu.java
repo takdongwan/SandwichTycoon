07d19291b30c05b16f4f3cd6f9d0f58b87a039de
takdongwan <tak46123@gamil.com>
2020-04-04 22:18:48 +0900

    tycoongame 게임시작 게임종료 화면전환버튼 구현  ,시작

---
+++ Project/src/project/SandwichMenu.java
@@ -0,0 +1,23 @@
+package project;
+
+public class SandwichMenu {
+	
+	
+	private String sandwichCase;
+
+	public String getSandwichCase() {
+		return sandwichCase;
+	}
+
+	public void setSandwichCase(String sandwichCase) {
+		this.sandwichCase = sandwichCase;
+	}
+
+	public SandwichMenu(String sandwichCase) {
+		super();
+		this.sandwichCase = sandwichCase;
+	}
+	
+	
+
+}
