ae3f1207cc91ec3c4aaa49564eb1a3f1bb9a3406
yunso <cw2cu96@gmail.com>
2020-04-20 17:33:43 +0900

    Add class Frame_trade Add class Frame_minigame

---
+++ Project/src/project/Frame_trade.java
@@ -0,0 +1,37 @@
+package project;
+
+import javax.swing.JFrame;
+
+public class Frame_trade extends JFrame {
+
+	public Frame_trade() {
+		setJFrame();
+		setJPanel();
+		setJLabel();
+		setJButton();
+	}
+	
+	public void setJFrame() {
+		setTitle("흥정하기");
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		setLayout(null);
+		getContentPane().setLayout(null);
+		setUndecorated(true); // 임의로 상점 창을 종료할 수 없도록 undecorated 설정
+		setVisible(true);
+	}
+	
+	public void setJPanel() {
+		
+	}
+	
+	public void setJLabel() {
+		
+	}
+	
+	public void setJButton() {
+		
+	}
+}
