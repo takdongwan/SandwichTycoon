6ee62d18eda38ce57ccd01145c7dfe8621def10d
yunso <cw2cu96@gmail.com>
2020-04-22 00:44:46 +0900

    Edit JLabel text in method setJLabel in class Frame_jumpGame

--- Project/src/project/Frame_jumpGame.java
+++ Project/src/project/Frame_jumpGame.java
@@ -26,7 +26,7 @@ public class Frame_jumpGame extends JFrame {
 
 	public void setJFrame() {
 
-		setTitle("미니게임");
+		setTitle("점프게임..");
 		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		setResizable(false);
 		setLocationRelativeTo(null);
@@ -48,21 +48,21 @@ public class Frame_jumpGame extends JFrame {
 
 	public void setJLabel() {
 
-		jumpGameInfo = new JLabel("재료상점");
+		jumpGameInfo = new JLabel("jumpGame");
 		jumpGameInfo.setVerticalAlignment(SwingConstants.TOP);
 		jumpGameInfo.setHorizontalAlignment(SwingConstants.CENTER);
 		jumpGameInfo.setFont(jumpGameInfo.getFont().deriveFont(20.0f)); // 폰트 사이즈 20
 		jumpGameInfo.setBounds(0, 40, Main.SCREEN_WIDTH, 30); // x좌표, y좌표, 너비, 높이
 		jumpGamePanel.add(jumpGameInfo);
 
-		explanation = new JLabel("구매를 원하는 만큼 각 재료를 클릭해주세요!");
+		explanation = new JLabel("스페이바를 눌러서 다가오는 장애물을 피하세요!");
 		explanation.setVerticalAlignment(SwingConstants.TOP);
 		explanation.setHorizontalAlignment(SwingConstants.CENTER);
 		explanation.setFont(explanation.getFont().deriveFont(12.0f)); // 폰트 사이즈 12
 		explanation.setBounds(0, 70, Main.SCREEN_WIDTH, 30); // x좌표, y좌표, 너비, 높이
 		jumpGamePanel.add(explanation);
 		
-		scoreInfo = new JLabel("획득 골드: " + score);
+		scoreInfo = new JLabel("현재 점수: " + score);
 		scoreInfo.setVerticalAlignment(SwingConstants.TOP);
 		scoreInfo.setHorizontalAlignment(SwingConstants.CENTER);
 		scoreInfo.setFont(explanation.getFont().deriveFont(12.0f)); // 폰트 사이즈 12
